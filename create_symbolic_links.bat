3a7a927a3b1ad8e096386314c3b6c465985fb2f1
QuickDeck <105922353+QuickDeck@users.noreply.github.com>
2026-01-16 12:58:04 -0500

    Create symbolic link setup script

---
+++ create_symbolic_links.bat
@@ -0,0 +1,7 @@
+mklink /J "%HOMEDRIVE%\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data\Editor\Mods\PF2E_Spell_Expansion_47b3313e-8869-0b2d-cc65-0b2bd2cb2107" ".\Editor\Mods\PF2E_Spell_Expansion_47b3313e-8869-0b2d-cc65-0b2bd2cb2107"
+mklink /J "%HOMEDRIVE%\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data\Mods\PF2E_Spell_Expansion_47b3313e-8869-0b2d-cc65-0b2bd2cb2107" ".\Mods\PF2E_Spell_Expansion_47b3313e-8869-0b2d-cc65-0b2bd2cb2107"
+mklink /J "%HOMEDRIVE%\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data\Projects\PF2E_Spell_Expansion_47b3313e-8869-0b2d-cc65-0b2bd2cb2107" ".\Projects\PF2E_Spell_Expansion_47b3313e-8869-0b2d-cc65-0b2bd2cb2107"
+mklink /J "%HOMEDRIVE%\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data\Public\PF2E_Spell_Expansion_47b3313e-8869-0b2d-cc65-0b2bd2cb2107" ".\Public\PF2E_Spell_Expansion_47b3313e-8869-0b2d-cc65-0b2bd2cb2107"
+mklink /J "%HOMEDRIVE%\Program Files (x86)\Steam\steamapps\common\Baldurs Gate 3\Data\Generated\Public\PF2E_Spell_Expansion_47b3313e-8869-0b2d-cc65-0b2bd2cb2107" ".\Generated\Public\PF2E_Spell_Expansion_47b3313e-8869-0b2d-cc65-0b2bd2cb2107"
+
+pause
